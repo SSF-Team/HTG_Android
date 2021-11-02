bd12377bde5055c2a3cf2817508e598dd548e846
Stapxs <1007028430.stapx@gmail.com>
2021-11-02 11:17:18 +0800

    :sparkles: Add Dock Bar and Sidebar

--- app/src/main/java/com/chuhelan/htg/MainActivity.java
+++ app/src/main/java/com/chuhelan/htg/MainActivity.java
@@ -2,7 +2,9 @@ package com.chuhelan.htg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 跳转去主页
+        Intent main = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(main);   // 启动
+        this.finish();    // 关闭自身防止返回
     }
 }
